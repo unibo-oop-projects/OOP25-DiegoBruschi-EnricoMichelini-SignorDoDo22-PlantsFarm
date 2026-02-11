aab38e14602774ae69ec2a24124753b270c069f7
SignorDoDo22 <francesco.fusillo3@studio.unibo.it>
2026-02-11 17:11:07 +0100

    Finishing  spotbugs.

--- src/main/java/it/unibo/plantsfarm/view/animation/api/SelectorFrames.java
+++ src/main/java/it/unibo/plantsfarm/view/animation/api/SelectorFrames.java
@@ -1,7 +1,6 @@
 package it.unibo.plantsfarm.view.animation.api;
 
 import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
 import it.unibo.plantsfarm.controller.gamepanel.api.ControllerGamePanel.UserInput;
 
 /**
@@ -23,13 +22,6 @@ public interface SelectorFrames {
      * @param now Current time in milliseconds
      */
     void update(Long now);
-
-    /**
-     * Returns the current Frame of the player.
-     *
-     * @return CurrentFrame
-     */
-    BufferedImage getCurrentImage();
 
     void render(Graphics2D g2d, double posPlayerx, double posPlayery, int camerax, int cameray);
 
